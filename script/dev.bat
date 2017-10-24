fbbd1a88e137fe12b84895d7468df8bc34b769b9
hbc <670762853@qq.com>
2017-10-24 16:06:50 +0000

    update

--- script/dev.bat
+++ script/dev.bat
@@ -3,7 +3,7 @@ taskkill /f /im werl.exe
 start start_gate.bat
 start start_auth.bat
 start start_center.bat
-start start_connector_1.bat
-start start_connector_2.bat
-start start_logic_1.bat
-start start_logic_2.bat
+rem start start_connector_1.bat
+rem start start_connector_2.bat
+rem start start_logic_1.bat
+rem start start_logic_2.bat
